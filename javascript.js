28ee963a055fd5a33b97e0df316d717933a920af
Pab-83 <gabrielpablo8344@gmail.com>
2022-10-07 13:30:14 -0300

    add score system

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 let choices = ["rock", "paper", "scissors"];
+player_score = 0;
+computer_score = 0;
 
 function computer_selector() {
     let choice = Math.floor(Math.random() * choices.length);
@@ -12,20 +14,16 @@ function play_round(computer_choice, player_choice) {
     };
     
     if (computer_choice == "scissors" && player_choice == "rock") {
-        console.log("Computer Choice: " + computer_choice + "\nPlayer Choice: " + player_choice);
         return "player_wins";
     }
     else if (computer_choice == "rock" && player_choice == "scissors") {
-        console.log("Computer Choice: " + computer_choice + "\nPlayer Choice: " + player_choice);
         return "computer_wins";
     }
     
     else if (choices.indexOf(computer_choice) < choices.indexOf(player_choice)) {
-        console.log("Computer Choice: " + computer_choice + "\nPlayer Choice: " + player_choice);
         return "player_wins";
     }
     else if (choices.indexOf(player_choice) < choices.indexOf(computer_choice)) {
-        console.log("Computer Choice: " + computer_choice + "\nPlayer Choice: " + player_choice);
         return "computer_wins";
     };
 }
@@ -33,6 +31,32 @@ function play_round(computer_choice, player_choice) {
 for (i = 0; i <= 4; i++) {
     let computer_choice = computer_selector();
     let player_choice = prompt("Choose: Rock, Paper, Scissors").toLowerCase();
-    console.log(play_round(computer_choice, player_choice));
+    winner = play_round(computer_choice, player_choice);
+
+    if (winner == "player_wins") {
+        player_score += 1;
+        console.log("Player choose: " + player_choice + "\nThe Computer choose: " + computer_choice);
+        console.log("Player wins");
+    }
+    else if (winner == "computer_wins") {
+        computer_score += 1;
+        console.log("Player choose: " + player_choice + "\nThe Computer choose: " + computer_choice);
+        console.log("Computer wins");
+    }
+    else {
+        console.log("Draw");
+    }
+
+    console.log("Player Score: " + player_score + " Computer Score: " + computer_score);
 };
+
+if (player_score > computer_score) {
+    console.log("Player wins the match!");
+}
+else if (computer_score > player_score) {
+    console.log("Computer wins the match!");
+}
+else {
+    console.log("It's a Draw!");
+}
 
